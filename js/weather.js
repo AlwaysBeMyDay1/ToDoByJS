ea02168f98e46c3ca57bcded952ab343623dc931
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2021-12-24 23:50:50 +0900

    1224

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,7 @@ const city = document.querySelector("#weather span:last-child")
 const API_KEY = '1dbd59baf29415c1ff9bb3d4bfa229f4';
 
 function onGeoOK(position){
+    console.log(position);
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
